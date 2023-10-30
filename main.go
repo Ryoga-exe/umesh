ac797b788a2c024833d1faac54d72c9d99a5e433
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-10-30 19:06:11 +0900

    feat: implement cd, exit and execCommand

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+
+	buitinCommands := map[string]func([]string) error{
+		"cd":   changeDirectory,
+		"exit": exit,
+	}
+
 	for {
 		pwd, _ := os.Getwd()
 		fmt.Print(pwd, "> ")
@@ -20,16 +27,101 @@ func main() {
 		}
 
 		args := strings.Split(string(line), " ")
-		var procAttr os.ProcAttr
-		procAttr.Files = []*os.File{nil, os.Stdout, os.Stderr}
 
-		process, err := os.StartProcess(args[0], args, &procAttr)
+		buitinCommand, ok := buitinCommands[args[0]]
+
+		if ok {
+			err = buitinCommand(args)
+		} else {
+			err = execCommand(args)
+		}
+	}
+}
+
+func changeDirectory(args []string) (err error) {
+	var dir string
+	argc := len(args)
+	if argc == 1 {
+		dir, err = os.UserHomeDir()
 		if err != nil {
 			log.Fatal(err)
 		}
+	} else if argc == 2 {
+		dir = args[1]
+	} else {
+		return fmt.Errorf("5s: too many arguments", "cd")
+	}
+	return os.Chdir(dir)
+}
 
-		if _, err = process.Wait(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
+func exit(args []string) (err error) {
+	os.Exit(0)
+	return nil
+}
+
+func execCommand(args []string) (err error) {
+	cmd, err := absPathWithPATH(string(args[0]))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	log.Printf("command %s\n", cmd)
+
+	var procAttr os.ProcAttr
+	procAttr.Files = []*os.File{nil, os.Stdout, os.Stderr}
+
+	process, err := os.StartProcess(cmd, args, &procAttr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err = process.Wait(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	return nil
+}
+
+func absPathWithPATH(target string) (targetAbsPath string, err error) {
+	targetFileName := filepath.Base(target)
+	log.Printf("target %s\n", target)
+	log.Printf("targetFileName %s\n", targetFileName)
+
+	// if the specified string is a path
+	if target != targetFileName {
+		if filepath.IsAbs(target) {
+			// if absolute path
+			targetAbsPath = target
+		} else {
+			// if relative path
+			targetAbsPath, err = filepath.Abs(target)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if Exists(targetAbsPath) {
+			return targetAbsPath, nil
+		} else {
+			return "", fmt.Errorf("%s: no such file or directory", targetAbsPath)
 		}
 	}
+
+	// if the specified string is a file name
+
+	// find from $PATH
+	for _, path := range filepath.SplitList(os.Getenv("PATH")) {
+		log.Printf("%s\n", path)
+		targetAbsPath = filepath.Join(path, targetFileName)
+		if Exists(targetAbsPath) {
+			log.Printf("find in PATH %s\n", targetAbsPath)
+			return targetAbsPath, nil
+		}
+	}
+	return "", fmt.Errorf("%s: no such file or directory", targetFileName)
+}
+
+func Exists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
 }
